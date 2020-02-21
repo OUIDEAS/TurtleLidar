221fcc8e434c7beef601f6b7fdf3f6fe99194d70
Theo T <47221779+sch1ldkrote@users.noreply.github.com>
2020-02-21 17:47:34 -0500

    Changed Library, added functions
    Changed Library from adafruit to generic?
    
    Added functions in GImbalLidarControlClass

--- GimbalLidarControlClass.py
+++ GimbalLidarControlClass.py
@@ -1,8 +1,7 @@
-from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_StepperMotor
+from Raspi_MotorHAT import Raspi_MotorHAT
 import numpy as np
 from rplidar import RPLidar
 from gpiozero import Button
-import time
 
 
 class LidarGimbal():
@@ -12,18 +11,18 @@ class LidarGimbal():
         self.MM2INCH = 1 / 25.4
 
         self.lidar = RPLidar(PortName)
-        self.mh = Adafruit_MotorHAT()
+        self.mh = Raspi_MotorHAT(0x6F)
 
-        self.PanStepper = self.mh.getStepper(200, 1)
+        self.PanStepper = self.mh.getStepper(200, 2)
         self.PanStepper.setSpeed(10)
-        self.TiltStepper = self.mh.getStepper(200, 2)
+        self.TiltStepper = self.mh.getStepper(200, 1)
         self.TiltStepper.setSpeed(10)
 
     def turnOffMotors(self):
-        self.mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
-        self.mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
-        self.mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
-        self.mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+        self.mh.getMotor(1).run(Raspi_MotorHAT.RELEASE)
+        self.mh.getMotor(2).run(Raspi_MotorHAT.RELEASE)
+        self.mh.getMotor(3).run(Raspi_MotorHAT.RELEASE)
+        self.mh.getMotor(4).run(Raspi_MotorHAT.RELEASE)
 
     def turnOffLidar(self):
         self.lidar.stop()
@@ -35,6 +34,12 @@ class LidarGimbal():
         self.turnOffMotors()
         quit()
 
+    def lidarHealth(self):
+        info = self.lidar.get_info()
+        print(info)
+        health = self.lidar.get_health()
+        print(health)
+
     def homeLidar(self, panPin, tiltPin):
         # This needs rewritten/finished
         panbtn = Button(panPin)
@@ -42,10 +47,10 @@ class LidarGimbal():
 
         print("Homing...")
         while not panbtn.is_pressed():
-            self.PanStepper.step(1, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
+            self.PanStepper.step(1, Raspi_MotorHAT.BACKWARD, Raspi_MotorHAT.MICROSTEP)
         print("...")
         while not tiltbtn.is_pressed():
-            self.TiltStepper.step(1, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
+            self.TiltStepper.step(1, Raspi_MotorHAT.BACKWARD, Raspi_MotorHAT.MICROSTEP)
         print("...Homed")
 
     def zeroLidar(self):
@@ -68,7 +73,7 @@ class LidarGimbal():
                     # Need Error Function
                     Error = 0
                     if Error != 0:
-                        self.PanStepper.step(1, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.MICROSTEP)
+                        self.PanStepper.step(1, Raspi_MotorHAT.FORWARD, Raspi_MotorHAT.MICROSTEP)
                     else:
                         panZero = True
 
@@ -76,7 +81,7 @@ class LidarGimbal():
                     # Need Error Function
                     Error = 0
                     if Error != 0:
-                        self.TiltStepper.step(1, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.MICROSTEP)
+                        self.TiltStepper.step(1, Raspi_MotorHAT.FORWARD, Raspi_MotorHAT.MICROSTEP)
                     else:
                         tiltZero = True
                 else:
@@ -85,7 +90,7 @@ class LidarGimbal():
         except Exception as e:
             print("Stopping due to error:", e)
 
-    def lidarscan(self, path):
+    def lidarScanWrite(self, path, NumberOfScans):
         outfile = open(path, 'w')
         i = 0
 
@@ -96,16 +101,35 @@ class LidarGimbal():
                         i += 1
                     line = '\t'.join(str(measurment))
                     outfile.write(line + '\n')
-                if i > 20:
+                if i > NumberOfScans:
                     break
 
         except Exception as e:
             print("Stopping due to error:", e)
+
+        outfile.close()
+
+    def lidarScan(self, NumberOfScans):
+        i = 0
+        data = []
+
+        try:
+            for scan in self.lidar.iter_measurments():
+                data.append(np.array(scan))
+                for scandata in scan:
+                    if scandata[1]:
+                        i += 1
+                if i > NumberOfScans:
+                    break
+        except Exception as e:
+            print("Stopping due to error:", e)
+
+        return data
 
 
 if __name__ == "__main__":
     lg = LidarGimbal("COM9")
 
     lg.zeroLidar()
-    lg.lidarscan("Scan.txt")
+    lg.lidarScanWrite("Scan.txt")
     lg.shutdown()
---
+++ PiGimbalControl.py
@@ -0,0 +1,45 @@
+from Raspi_MotorHAT import Raspi_MotorHAT
+import time
+import atexit
+
+mh = Raspi_MotorHAT(0x6F)
+
+def turnOffMotors():
+    mh.getMotor(1).run(Raspi_MotorHAT.RELEASE)
+    mh.getMotor(2).run(Raspi_MotorHAT.RELEASE)
+    mh.getMotor(3).run(Raspi_MotorHAT.RELEASE)
+    mh.getMotor(4).run(Raspi_MotorHAT.RELEASE)
+
+
+def run():
+    # Stepper Motors
+    PanStepper = mh.getStepper(200, 2)
+    PanStepper.setSpeed(10)
+    TiltStepper = mh.getStepper(200, 1)
+    TiltStepper.setSpeed(10)
+
+    try:
+        while True:
+            pan = int(input("Pan how many steps: "))
+            if pan > 0:
+                PanStepper.step(pan, Raspi_MotorHAT.FORWARD, Raspi_MotorHAT.MICROSTEP)
+            else:
+                PanStepper.step(pan, Raspi_MotorHAT.BACKWARD, Raspi_MotorHAT.MICROSTEP)
+
+            tilt = int(input("Tilt how many steps: "))
+            if tilt > 0:
+                TiltStepper.step(tilt, Raspi_MotorHAT.FORWARD, Raspi_MotorHAT.MICROSTEP)
+            else:
+                TiltStepper.step(tilt, Raspi_MotorHAT.BACKWARD, Raspi_MotorHAT.MICROSTEP)
+            stop = int(input("Type 7 to stop: "))
+            if stop == 7:
+                break
+
+    except Exception as e:
+        print("Stopping due to error:", e)
+        turnOffMotors()
+
+
+if __name__ == '__main__':
+    run()
+    turnOffMotors()

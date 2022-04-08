79d15658d460fd9bb4af6deb8d967213563054ea
Theo <tt431915@ohio.edu>
2022-04-08 15:59:50 -0400

    Prevent duplicate lidar data entries

--- LidarClass.py
+++ LidarClass.py
@@ -12,6 +12,7 @@ class RPLidarClass:
         print("launch")
 
         self.scan_data = None
+        self.scan_count = 0
 
         uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
         roslaunch.configure_logging(uuid)
@@ -29,26 +30,39 @@ class RPLidarClass:
 
     def get_scan(self, LaserScan):
         self.scan_data = LaserScan
+        self.scan_count = self.scan_count + 1
 
     def get_lidar_data(self, t):
+        last_scan = -1
+        numScan = 0
         rospy.sleep(5)
         tscan = time.time()
         ang = np.array([])
         dis = np.array([])
-        while time.time() - tscan <= t:
+        while True:
             if self.scan_data is not None:
-                msg = self.scan_data
-                ranges = msg.ranges
-                angles_min = msg.angle_min
-                angles_max = msg.angle_max
-                angle_inc = msg.angle_increment
-                angle_array = np.linspace(angles_min, angles_max, np.size(ranges))
-                ranges = np.array(ranges)
-                ang = np.append(ang, angle_array[np.isfinite(ranges)])
-                dis = np.append(dis, ranges[np.isfinite(ranges)])
+                if self.scan_count > last_scan:
+                    last_scan = self.scan_count
+                    numScan = numScan + 1
+
+                    msg = self.scan_data
+                    ranges = msg.ranges
+                    angles_min = msg.angle_min
+                    angles_max = msg.angle_max
+                    angle_inc = msg.angle_increment
+                    angle_array = np.linspace(angles_min, angles_max, np.size(ranges))
+                    ranges = np.array(ranges)
+                    ang = np.append(ang, angle_array[np.isfinite(ranges)])
+                    dis = np.append(dis, ranges[np.isfinite(ranges)])
             else:
                 print("No Data?")
                 rospy.sleep(.2)
+
+            if time.time() - tscan >= (t * 2):
+                print("Lidar Scan Timeout")
+                break
+            elif numScan > t * 5:
+                break
             rospy.sleep(.1)
 
         return np.rad2deg(ang), dis*1000
